8b9ff2af59a7b35de913bc779bf703d493a062da
kobaserg <kobasts@gmail.com>
2022-09-02 12:34:05 +0200

    ДЗ9 задание 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const bodyBlock = document.querySelector('body');
 
+let timerId = null;
+btnStop.setAttribute('disabled', true);
+btnStart.style.cursor = 'pointer';
+btnStop.style.cursor = 'default';
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onChangeBodyColor() {
+  bodyBlock.style.backgroundColor = getRandomHexColor();
+}
+
+function onStatusButton(activButton, inActiveButton) {
+  inActiveButton.setAttribute('disabled', true);
+  activButton.removeAttribute('disabled');
+  activButton.style.cursor = 'pointer';
+  inActiveButton.style.cursor = 'default';
+}
+
+btnStart.addEventListener('click', () => {
+  onStatusButton(btnStop, btnStart);
+  timerId = setInterval(onChangeBodyColor, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+  onStatusButton(btnStart, btnStop);
+  clearInterval(timerId);
+});
